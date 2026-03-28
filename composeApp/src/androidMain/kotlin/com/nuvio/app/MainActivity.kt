66e9ce37591ce00ab9481843a2375c34ae73bc9e
tapframe <85391825+tapframe@users.noreply.github.com>
2026-03-28 21:40:58 +0530

    feat: Remove status bar color and light status bar settings from MainActivity and themes

--- composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nuvio/app/MainActivity.kt
@@ -17,9 +17,6 @@ import com.nuvio.app.features.watchprogress.WatchProgressStorage
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(
-                scrim = 0xFF020404.toInt(),
-            ),
             navigationBarStyle = SystemBarStyle.dark(
                 scrim = 0xFF020404.toInt(),
             ),
@@ -27,7 +24,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         window.setBackgroundDrawableResource(R.color.nuvio_background)
         window.navigationBarColor = getColor(R.color.nuvio_background)
-        window.statusBarColor = getColor(R.color.nuvio_background)
         window.isNavigationBarContrastEnforced = false
         AddonStorage.initialize(applicationContext)
         LibraryStorage.initialize(applicationContext)
